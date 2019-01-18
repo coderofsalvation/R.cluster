59ef079203739b0c54985f4ad81b2e158bca1e65
Leon <info@leon.vankammen.eu>
2019-01-18 10:41:49 +0100

    work in progress [might break]

--- boot.sh
+++ boot.sh
@@ -1,6 +1,9 @@
 # run scripts in 'script'-folder 
 ls script/*.R | xargs Rscript &
 
+# publish www-folder to website
+echo "$GH_KEY" > ~/.ssh/id_rsa.gh
+git remote set-url origin git@github.com:coderofsalvation/R.cluster.git
 git subtree push --prefix www origin gh-pages
 
 { sleep 10m; echo "process took longer than 10 mins"; exit 1; } &
